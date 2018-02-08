ca39b37a7fb2e03d0eb8330a2c35108c5d1e9850
Guillaume <yom7@hotmail.fr>
2018-02-08 22:26:09 +0100

    try to use the 'iter' method instead of 'findall'

--- svgguru.py
+++ svgguru.py
@@ -199,6 +199,11 @@ def iter_multiplefindall(tree, xpathlist, ns):
         for node in tree.findall(xpath, ns):
             yield node
 
+def iter_multiplefindall(tree, xpathlist, ns):
+    for xpath in xpathlist:
+        for node in tree.iter(xpath, ns):
+            yield node
+
 
 def change_all_attr(tree, taglist, attrname, func, *funcargs):
     """transform all corresponding attributes in the tree with the function.
